f7f3153eeaf499c5a0d79299b757a7817772661b
Lương Chung Hội <87046628+lchryu@users.noreply.github.com>
2023-08-26 10:15:17 +0700

    Day1 - init project

---
+++ index.py
@@ -0,0 +1,4 @@
+print("day1")
+# git add .  checking tất cả những thay đổi trong project của mình
+# git commit -m "init project"  --> viết message/description cho lần commit 
+# git push  --> đẩy lên cloud
